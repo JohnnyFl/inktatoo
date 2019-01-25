de276206db62666b9771934d1a24498715053d6d
Thomas_NeFeX <jakediego01@gmail.com>
2019-01-25 19:21:35 +0200

    Updates

---
+++ precache-manifest.23235a1a85c22a51ac45504f3dbf8e98.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "968f9d78b23f83e6062cb8fbdae217a9",
+    "url": "/inktatoo/static/media/6.968f9d78.png"
+  },
+  {
+    "revision": "010867f9931ecded77fe",
+    "url": "/inktatoo/static/css/main.30c2b6ca.chunk.css"
+  },
+  {
+    "revision": "addc2bd0cf9f65db9f02",
+    "url": "/inktatoo/static/js/1.addc2bd0.chunk.js"
+  },
+  {
+    "revision": "59a08b280da0d498cd6b",
+    "url": "/inktatoo/static/js/runtime~main.59a08b28.js"
+  },
+  {
+    "revision": "aeb9c32152c1060e146efee009761a14",
+    "url": "/inktatoo/static/media/img1.aeb9c321.png"
+  },
+  {
+    "revision": "79aa329a4737fbeb9807650f4824df4c",
+    "url": "/inktatoo/static/media/img2.79aa329a.png"
+  },
+  {
+    "revision": "1f01e9f2b43dddbe3762a0396ec55105",
+    "url": "/inktatoo/static/media/img3.1f01e9f2.png"
+  },
+  {
+    "revision": "1a8a7dfedaff65f9fbc827ebbd25982e",
+    "url": "/inktatoo/static/media/img4.1a8a7dfe.png"
+  },
+  {
+    "revision": "595b8ccec0d19c6fffe950550f1bfafb",
+    "url": "/inktatoo/static/media/img5.595b8cce.png"
+  },
+  {
+    "revision": "48d0e84b66a8bf949578e11b1c74cfc6",
+    "url": "/inktatoo/static/media/img6.48d0e84b.png"
+  },
+  {
+    "revision": "4ab7a017ce74051ea30f7231025bea7b",
+    "url": "/inktatoo/static/media/img7.4ab7a017.png"
+  },
+  {
+    "revision": "d90aaa942e7f0e3835a29da6e9823421",
+    "url": "/inktatoo/static/media/img8.d90aaa94.png"
+  },
+  {
+    "revision": "fb7a6ad2dfb520dc27732c1e93464106",
+    "url": "/inktatoo/static/media/img9.fb7a6ad2.png"
+  },
+  {
+    "revision": "468bf3df896dbbbefa367de7c2b0bf9d",
+    "url": "/inktatoo/static/media/img10.468bf3df.png"
+  },
+  {
+    "revision": "660937d3c5e974639a586b688b5681e8",
+    "url": "/inktatoo/static/media/img11.660937d3.png"
+  },
+  {
+    "revision": "b5273e128db4b715a9eb987b7b791768",
+    "url": "/inktatoo/static/media/img12.b5273e12.png"
+  },
+  {
+    "revision": "3c457b0dc4636c1e11805b520493bc44",
+    "url": "/inktatoo/static/media/1.3c457b0d.png"
+  },
+  {
+    "revision": "e3f8359d7701607ddb3f25b36663d7b0",
+    "url": "/inktatoo/static/media/2.e3f8359d.png"
+  },
+  {
+    "revision": "4cdd9d818c54d42b0b7d405aa6fe04d5",
+    "url": "/inktatoo/static/media/3.4cdd9d81.png"
+  },
+  {
+    "revision": "759203cf2dfd220b60de7e954bbeffef",
+    "url": "/inktatoo/static/media/4.759203cf.png"
+  },
+  {
+    "revision": "61a1039665847144b59e9d5d07604b31",
+    "url": "/inktatoo/static/media/5.61a10396.png"
+  },
+  {
+    "revision": "010867f9931ecded77fe",
+    "url": "/inktatoo/static/js/main.010867f9.chunk.js"
+  },
+  {
+    "revision": "aa3fe26e4a791031e546fb39e8c50435",
+    "url": "/inktatoo/static/media/7.aa3fe26e.png"
+  },
+  {
+    "revision": "7a5979f34813d6be57c1911797dcb3d6",
+    "url": "/inktatoo/static/media/8.7a5979f3.png"
+  },
+  {
+    "revision": "fef9511d1e60d2649958ac485c18d8d7",
+    "url": "/inktatoo/static/media/9.fef9511d.png"
+  },
+  {
+    "revision": "0f8ee0c1e2c89fac298878bcfadeaf88",
+    "url": "/inktatoo/static/media/10.0f8ee0c1.png"
+  },
+  {
+    "revision": "90d0a722eb9950ee0d8f004389c6f38f",
+    "url": "/inktatoo/static/media/11.90d0a722.png"
+  },
+  {
+    "revision": "7e1e7eaf8f84e4fe2dcf6bbb4234e384",
+    "url": "/inktatoo/static/media/12.7e1e7eaf.png"
+  },
+  {
+    "revision": "76fafdf2ab9fe05cb4e622a00f106e9e",
+    "url": "/inktatoo/static/media/13.76fafdf2.png"
+  },
+  {
+    "revision": "c8eb5cadd195bafe7691f5abca2ec33a",
+    "url": "/inktatoo/static/media/14.c8eb5cad.png"
+  },
+  {
+    "revision": "9ff11657bcd47204c94ae39aea1791cc",
+    "url": "/inktatoo/static/media/15.9ff11657.png"
+  },
+  {
+    "revision": "f8c0c57eb2600dcf1b2bb59c63419b0a",
+    "url": "/inktatoo/static/media/16.f8c0c57e.png"
+  },
+  {
+    "revision": "c3d02f4b8fb04b58ca95d0bdcf831aa0",
+    "url": "/inktatoo/static/media/17.c3d02f4b.png"
+  },
+  {
+    "revision": "211240058bc3fc135b6eeaa8c4b117b5",
+    "url": "/inktatoo/static/media/18.21124005.png"
+  },
+  {
+    "revision": "b90b57921d28a8f5892fa85f1a4eccc5",
+    "url": "/inktatoo/static/media/header.b90b5792.png"
+  },
+  {
+    "revision": "358624cdab36e615b676103c7122ea88",
+    "url": "/inktatoo/static/media/Livello_26.358624cd.png"
+  },
+  {
+    "revision": "343ceda1b928bcff621663f50a0d8b0b",
+    "url": "/inktatoo/static/media/img_studio_bg.343ceda1.png"
+  },
+  {
+    "revision": "4e5789fdbbc11d3d322aa9559d02f250",
+    "url": "/inktatoo/static/media/team_bg.4e5789fd.png"
+  },
+  {
+    "revision": "71d92eb3fb9881ecb050262fdb2cfff4",
+    "url": "/inktatoo/static/media/img_bg.71d92eb3.png"
+  },
+  {
+    "revision": "9a803066943534d2a17fc8a2c6cf5019",
+    "url": "/inktatoo/static/media/correy.9a803066.png"
+  },
+  {
+    "revision": "d5a2bbdaf88e8ce62e189fa64a9d7113",
+    "url": "/inktatoo/static/media/kat.d5a2bbda.png"
+  },
+  {
+    "revision": "8b1e8d72ceff72c8de5d85f83751f5b9",
+    "url": "/inktatoo/index.html"
+  }
+];
